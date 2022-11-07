65e240f1a4e0ba0d31f173d5755c03cddaa01e21
Juan Guerrero <JCystems@gMail.com>
2022-11-07 10:56:22 -0600

    test(coverage)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reporters: ['default', 'jest-junit'],
+  collectCoverage: true,
+  coverageReporters: ['cobertura'],
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 };
